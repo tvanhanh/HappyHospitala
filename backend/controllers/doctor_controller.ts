e52f6779e4094c4a4e479cefd1c3a0478fd17663
Noofg <nonofg15@gmail.com>
2026-06-02 20:50:48 +0700

    Fix login bug

--- backend/controllers/doctor_controller.ts
+++ backend/controllers/doctor_controller.ts
@@ -136,7 +136,7 @@ export const getDoctorsByDepartment = async (
 ) => {
   try {
     const { departmentId } = req.params;
-    console.log("Department =", req.params.departmentId);
+  git add .console.log("Department =", req.params.departmentId);
     const doctors = await User.find({
       role: "doctor",
       departmentId: departmentId,
